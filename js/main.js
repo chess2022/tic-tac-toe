d36fb21a7224b51f213854b0a1ea1349992b7281
Cheryl Weigel <cheryl.weigel@gmail.com>
2022-03-15 20:29:28 -0700

    adds reset button markup and styling

--- js/main.js
+++ js/main.js
@@ -1,9 +1,19 @@
 console.log("js is up and running!");
 /*----- constants -----*/
+// X & O - players
+// winning combinations
 
 /*----- app's state (variables) -----*/
+// where the players click (add an X or O) - make sure square is not taken first
+// whose turn is it
+// when we have a winning combo or if there is a tie
+// bonus - if the player wants to quit
+// bonus - add score feature for # of games won
+// bonus - make a timer for each player's turn
 
 /*----- cached element references -----*/
+// the game squares
+// reset button
 
 /*----- event listeners -----*/
 
