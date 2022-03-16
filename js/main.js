29b3d4d2b04c8e63c35a124e6cef5ca724888b7b
Cheryl Weigel <cheryl.weigel@gmail.com>
2022-03-15 19:43:58 -0700

    Add gameboard and js pseudocode

---
+++ js/main.js
@@ -0,0 +1,10 @@
+console.log("js is up and running!");
+/*----- constants -----*/
+
+/*----- app's state (variables) -----*/
+
+/*----- cached element references -----*/
+
+/*----- event listeners -----*/
+
+/*----- functions -----*/
